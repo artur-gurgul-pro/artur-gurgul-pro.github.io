a80b5da7358b6837f50d08e75bc3f9da7a43c19e
Artoor Gurgul <artoor@lucyos.com>
2016-10-31 11:31:55 +0100

    update

--- run-server.sh
+++ run-server.sh
@@ -3,5 +3,9 @@
 sudo mount -t ramfs -o size=16m ramfs /mnt/ram-blog
 bundle exec jekyll serve
 
+bundle exec jekyll build
+
 http://blog.teamtreehouse.com/using-github-pages-to-host-your-website
 
+http://jekyllthemes.org/themes/gatok/
+https://jekyllthemes.io/theme/12330472/hpstr-jekyll-theme
